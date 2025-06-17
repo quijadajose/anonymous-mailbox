ca0ff44b58caa5ad9a49b740e6fcec7f151ba113
José <57696252+quijadajose@users.noreply.github.com>
2025-06-17 14:27:22 -0400

    Update schema.sql
    Remove demmo sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,2 @@
-DROP TABLE IF EXISTS Customers;
-CREATE TABLE IF NOT EXISTS Customers (CustomerId INTEGER PRIMARY KEY, CompanyName TEXT, ContactName TEXT);
-INSERT INTO Customers (CustomerID, CompanyName, ContactName) VALUES (1, 'Alfreds Futterkiste', 'Maria Anders'), (4, 'Around the Horn', 'Thomas Hardy'), (11, 'Bs Beverages', 'Victoria Ashworth'), (13, 'Bs Beverages', 'Random Name');
-
 DROP TABLE IF EXISTS messages;
-
 CREATE TABLE messages (id INTEGER PRIMARY KEY AUTOINCREMENT, content TEXT NOT NULL, created_at TEXT NOT NULL);
